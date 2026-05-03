f69ade9d3e87c66841cb282809ed7a45d9823f8d
Saltator <saltatorv@gmail.com>
2026-05-03 21:44:49 +0200

    add landing page header

---
+++ src/app/components/headers/landing-page-header/landing-page-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Logo } from "../logo/logo";
+
+@Component({
+  selector: 'app-landing-page-header',
+  imports: [Logo],
+  templateUrl: './landing-page-header.html',
+  styleUrl: './landing-page-header.css',
+})
+export class LandingPageHeader {
+
+}
