9e3ca7c221e40a7baf7123022e92151c56575d67
Saltator <saltatorv@gmail.com>
2026-05-09 23:33:24 +0200

    create index-layout-component

---
+++ src/app/components/layouts/index-layout-component/index-layout-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LandingPageHeader } from "../../headers/landing-page-header/landing-page-header";
+
+@Component({
+  selector: 'app-index-layout-component',
+  imports: [LandingPageHeader],
+  templateUrl: './index-layout-component.html',
+  styleUrl: './index-layout-component.css',
+})
+export class IndexLayoutComponent {
+
+}
