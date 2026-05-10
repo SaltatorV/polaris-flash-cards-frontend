7cbb1c58af352e54e52114558003569d23e66473
Saltator <saltatorv@gmail.com>
2026-05-10 11:23:28 +0200

    update landing page header with menu

--- src/app/components/headers/landing-page-header/landing-page-header.ts
+++ src/app/components/headers/landing-page-header/landing-page-header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Logo } from "../logo/logo";
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-landing-page-header',
-  imports: [Logo],
+  imports: [Logo, MatToolbar, MatToolbarRow],
   templateUrl: './landing-page-header.html',
   styleUrl: './landing-page-header.css',
 })
