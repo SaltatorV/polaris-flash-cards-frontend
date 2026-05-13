f171568daf7b4960e037e18e0041eeb5a49a98f7
Saltator <saltatorv@gmail.com>
2026-05-14 00:00:45 +0200

    generate dashboard-page-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
-import { Routes } from '@angular/router';
-import { LandingPageLayout } from './components/layouts/landing-page-layout/landing-page-layout';
+import {Routes} from '@angular/router';
+import {LandingPageLayout} from './components/layouts/landing-page-layout/landing-page-layout';
+import {DashboardPageLayout} from './components/layouts/dashboard-page-layout/dashboard-page-layout';
 
 export const routes: Routes = [
-  { path: '', component: LandingPageLayout },
+  {path: '', component: LandingPageLayout},
+  {path: 'login', component: DashboardPageLayout}
 ];
---
+++ src/app/components/layouts/dashboard-page-layout/dashboard-page-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page-layout',
+  imports: [],
+  templateUrl: './dashboard-page-layout.html',
+  styleUrl: './dashboard-page-layout.css',
+})
+export class DashboardPageLayout {
+
+}
