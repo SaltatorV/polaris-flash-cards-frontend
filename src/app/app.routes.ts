55ab0781e21632eda50bc94274c49b34069f4ed1
Saltator <saltatorv@gmail.com>
2026-05-14 22:48:30 +0200

    generate register-page-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {Routes} from '@angular/router';
 import {LandingPageLayout} from './components/layouts/landing-page-layout/landing-page-layout';
 import {DashboardPageLayout} from './components/layouts/dashboard-page-layout/dashboard-page-layout';
 import {LoginPageLayout} from './components/layouts/login-page-layout/login-page-layout';
+import {RegisterPageLayout} from './components/layouts/register-page-layout/register-page-layout';
 
 export const routes: Routes = [
   {path: '', component: LandingPageLayout},
   {path: 'login', component: LoginPageLayout},
+  {path: 'register', component: RegisterPageLayout},
   {path: 'dashboard', component: DashboardPageLayout}
 ];
---
+++ src/app/components/layouts/register-page-layout/register-page-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page-layout',
+  imports: [],
+  templateUrl: './register-page-layout.html',
+  styleUrl: './register-page-layout.css',
+})
+export class RegisterPageLayout {
+
+}
