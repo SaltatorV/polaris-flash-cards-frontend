8359e582a7a9880b632de1c6b89632c2a813d91d
Saltator <saltatorv@gmail.com>
2026-05-14 22:46:55 +0200

    generate login-page-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {LandingPageLayout} from './components/layouts/landing-page-layout/landing-page-layout';
 import {DashboardPageLayout} from './components/layouts/dashboard-page-layout/dashboard-page-layout';
+import {LoginPageLayout} from './components/layouts/login-page-layout/login-page-layout';
 
 export const routes: Routes = [
   {path: '', component: LandingPageLayout},
-  {path: 'login', component: DashboardPageLayout}
+  {path: 'login', component: LoginPageLayout},
+  {path: 'dashboard', component: DashboardPageLayout}
 ];
---
+++ src/app/components/layouts/login-page-layout/login-page-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page-layout',
+  imports: [],
+  templateUrl: './login-page-layout.html',
+  styleUrl: './login-page-layout.css',
+})
+export class LoginPageLayout {
+
+}
